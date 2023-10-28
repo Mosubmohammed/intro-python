bcc1214cf626359a49a1eebae6088ace943d77a1
Mosubmohammed <mosup.050@gmail.com>
2023-10-28 15:45:35 +0300

    .

--- main.py
+++ main.py
@@ -1,1 +1,47 @@
 print('hello')
+
+name="""
+mosab
+mophammed
+gazy
+"""
+print(name)
+
+num1=5
+print(num1)
+
+num2=2.5
+print(num2)
+
+listP=['a','b','c','d']
+print(listP)    
+
+tb=(11,22,334,55,66)
+print(tb)
+
+person={
+    'name':'mosab',
+    'age':20,
+    'country':'jordan',
+    'skiills':['cp','java']
+
+}
+print(person)
+print(person['skiills'])
+
+
+list=[1,2,3,4,5]
+for i in list:
+    print(i)
+
+x=23
+for i in range(0,14,3):
+    print(i)
+
+person={
+    'name':'mosab',
+    'age':20,
+    'country':'jordan'
+}
+for i in person:
+    print(i,':',person[i])
