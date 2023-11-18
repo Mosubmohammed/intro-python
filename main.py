5140d9bd69d20dc7754eb3138652dcf318721d05
Mosubmohammed <mosup.050@gmail.com>
2023-11-18 21:42:32 +0300

    .

--- main.py
+++ main.py
@@ -281,217 +281,217 @@
 # import re
 
 
-# allpersons=[]
-# person={}
+allpersons=[]
+person={}
 # listOfBooks=[
 #     {'name':'book1', 'author':'author1', 'pages':100,'price':100,'content':'this is the content of the book'},
 #     {'name':'book2', 'author':'author2', 'pages':200,'price':200,'content':'this is the content of the book'},
 #     {'name':'book3', 'author':'author3', 'pages':300,'price':300,'content':'this is the content of the book'}
 # ]
 
-# print("welcome to out app")
-# while True:
-#     print('__'*20)
-#     print("1-create account")
-#     print("2-login")
-#     print("3-exit")
-#     print('4-show all persons')
-#     choice=input('enter ur choice :')
-#     if choice=='1':
-#         name=input("Enter the user name : ")
-#         if allpersons !=[]:
-#             isExi=True
-#             while isExi:
-#                 for i in allpersons:
-#                     if name==i['name']:
-#                         print('this name is already exist')
-#                         name=input("Enter the user name : ")
-#                         isExi=True
-#                         break
-#                     else:
-#                         isExi=False
+print("welcome to out app")
+while True:
+    print('__'*20)
+    print("1-create account")
+    print("2-login")
+    print("3-exit")
+    print('4-show all persons')
+    choice=input('enter ur choice :')
+    if choice=='1':
+        name=input("Enter the user name : ")
+        if allpersons !=[]:
+            isExi=True
+            while isExi:
+                for i in allpersons:
+                    if name==i['name']:
+                        print('this name is already exist')
+                        name=input("Enter the user name : ")
+                        isExi=True
+                        break
+                    else:
+                        isExi=False
                     
-#         while True:
-#             try:
-#                 age =int(input( "enter the user age : "))
-#                 break
-#             except:
-#                 print("please enter a number for age")
+        while True:
+            try:
+                age =int(input( "enter the user age : "))
+                break
+            except:
+                print("please enter a number for age")
 
-#         job=input('enter the user job :')
-#         skills=[]
-#         while True:
-#             try:
-#                 counter2=int(input('enter the number of skills :'))
-#                 break
-#             except Exception as e:
-#                 print('please enter a valid number')
-#                 continue
+        job=input('enter the user job :')
+        skills=[]
+        while True:
+            try:
+                counter2=int(input('enter the number of skills :'))
+                break
+            except Exception as e:
+                print('please enter a valid number')
+                continue
 
-#         for i in range(counter2):
-#             print('enter the skills number ',i+1,':')
-#             skills.append(input())
+        for i in range(counter2):
+            print('enter the skills number ',i+1,':')
+            skills.append(input())
 
-#         uppercasse1=False
-#         lowercase=False
-#         numbers=False
-#         while True:
-#             password=input("Enter the user password : ")
-#             if len(password)>=8:
-#                 for i in password:
-#                     if i.isupper():
-#                         uppercasse1=True
-#                     elif i.islower():
-#                         lowercase=True
-#                     elif i.isdigit():
-#                         numbers=True
-#                 if uppercasse1 and lowercase and numbers:
-#                     print('the password is strong') 
-#                     break
-#                 else:
-#                     if uppercasse1!=True:
-#                         print('the password dont have a uppercase') 
-#                     if lowercase!=True:
-#                         print('the password dont have a lowercase')  
-#                     if numbers!=True:
-#                         print('the password dont have a numbers')           
+        uppercasse1=False
+        lowercase=False
+        numbers=False
+        while True:
+            password=input("Enter the user password : ")
+            if len(password)>=8:
+                for i in password:
+                    if i.isupper():
+                        uppercasse1=True
+                    elif i.islower():
+                        lowercase=True
+                    elif i.isdigit():
+                        numbers=True
+                if uppercasse1 and lowercase and numbers:
+                    print('the password is strong') 
+                    break
+                else:
+                    if uppercasse1!=True:
+                        print('the password dont have a uppercase') 
+                    if lowercase!=True:
+                        print('the password dont have a lowercase')  
+                    if numbers!=True:
+                        print('the password dont have a numbers')           
                     
-#             else:
-#                 print('the password is shorter than 8')                           
+            else:
+                print('the password is shorter than 8')                           
 
                             
-#         person={
-#             'name':name,
-#             'age':age,
-#             'job':job,
-#             'password':password
-#         }
-#         allpersons.append(person)
-#         print("you added person successfully")
-#     elif choice=='2': 
-#        print("__"*20)
-#        print('please enter ur name and ur password :')
-#        name=input('Enter the name :')
-#        password=input('Enter the password :')
-#        for i in allpersons:
-#            if i['name']==name and i['password']==password:
-#                print('welcome',name)
-#                userChoose=input('**u can update ur info do u wanna update it yes or no**')  
-#                if userChoose=='yes': 
-#                    print('1-name')
-#                    print('2-password')
-#                    print('3-job')
-#                    print('4-age')
-#                    print('5-Exit')
-#                    choice2=input('inter ur choice :')
-#                    if choice2=='1':
-#                        print('enter ur new name :')
-#                        person['name']=input()
-#                    elif choice2=='2':
-#                             print('enter ur new password :')
-#                             person['password']=input()
-#                    elif choice2=='3':
-#                        print('enter ur new job :')
-#                        person['job']=input()
-#                    elif choice2=='4':
-#                        print('enter ur new age :')
-#                        person['age']=input() 
+        person={
+            'name':name,
+            'age':age,
+            'job':job,
+            'password':password
+        }
+        allpersons.append(person)
+        print("you added person successfully")
+    elif choice=='2': 
+       print("__"*20)
+       print('please enter ur name and ur password :')
+       name=input('Enter the name :')
+       password=input('Enter the password :')
+       for i in allpersons:
+           if i['name']==name and i['password']==password:
+               print('welcome',name)
+               userChoose=input('**u can update ur info do u wanna update it yes or no**')  
+               if userChoose=='yes': 
+                   print('1-name')
+                   print('2-password')
+                   print('3-job')
+                   print('4-age')
+                   print('5-Exit')
+                   choice2=input('inter ur choice :')
+                   if choice2=='1':
+                       print('enter ur new name :')
+                       person['name']=input()
+                   elif choice2=='2':
+                            print('enter ur new password :')
+                            person['password']=input()
+                   elif choice2=='3':
+                       print('enter ur new job :')
+                       person['job']=input()
+                   elif choice2=='4':
+                       print('enter ur new age :')
+                       person['age']=input() 
                  
-#            else:
-#             print('the name or password is incorrect')
+           else:
+            print('the name or password is incorrect')
 
-#        print('do you wanna add new filed')
-#        userChoose2=input('Enter the yes or no :')
-#        if userChoose2=='yes':
-#             userfiled=input('please Enter your new filed type:')
-
-
-#             if userfiled=='int':
-#                     userfiled2=input('Enter the filed name :')
-#                     while True:
-#                         try:
-#                             userfilrdint=int(input('Enter the filed value :'))
-#                             # print(userfiled2+' is:  '+userfilrdint)
-#                             print('thanks for adding new filed') 
-#                             break
-#                         except Exception as e:
-#                             print('please enter a valid number') 
+       print('do you wanna add new filed')
+       userChoose2=input('Enter the yes or no :')
+       if userChoose2=='yes':
+            userfiled=input('please Enter your new filed type:')
 
 
-#             elif userfiled=='string':
-#                 userfiled3=input('Enter the filed name :')
-#                 userfilrdString=input('Enter the filed value :')
-#                 print(userfiled3+' is:  '+userfilrdString)
-#                 print('thanks for adding new filed')
+            if userfiled=='int':
+                    userfiled2=input('Enter the filed name :')
+                    while True:
+                        try:
+                            userfilrdint=int(input('Enter the filed value :'))
+                            # print(userfiled2+' is:  '+userfilrdint)
+                            print('thanks for adding new filed') 
+                            break
+                        except Exception as e:
+                            print('please enter a valid number') 
 
 
-#             elif userfiled=='list':
-#                     userfiled2=input('Enter the filed name :')
-#                     userChoose3=input('please int the type of ur list')
-#                     listUser=[]
+            elif userfiled=='string':
+                userfiled3=input('Enter the filed name :')
+                userfilrdString=input('Enter the filed value :')
+                print(userfiled3+' is:  '+userfilrdString)
+                print('thanks for adding new filed')
 
-#                     if userChoose3=='string':      
-#                         Usercounter=int(input('enter the number of ur ......:'))
-#                         for i in range(Usercounter):
-#                             print('please enter ur ..... ',i+1,':')
-#                             listUser.append(input())
-#                         print('thanks for adding new filed') 
-#                     elif userChoose3=='int':
+
+            elif userfiled=='list':
+                    userfiled2=input('Enter the filed name :')
+                    userChoose3=input('please int the type of ur list')
+                    listUser=[]
+
+                    if userChoose3=='string':      
+                        Usercounter=int(input('enter the number of ur ......:'))
+                        for i in range(Usercounter):
+                            print('please enter ur ..... ',i+1,':')
+                            listUser.append(input())
+                        print('thanks for adding new filed') 
+                    elif userChoose3=='int':
                         
-#                         Usercounter2=int(input('enter the number of ur ......:'))
-#                         for i in range(Usercounter2):
-#                             print('please enter ur ..... ',i+1,':')
-#                             listUser.append(input())
-#                         print('thanks for adding new filed') 
+                        Usercounter2=int(input('enter the number of ur ......:'))
+                        for i in range(Usercounter2):
+                            print('please enter ur ..... ',i+1,':')
+                            listUser.append(input())
+                        print('thanks for adding new filed') 
 
                         
-#                     elif userChoose2=='no':
-#                         break           
+                    elif userChoose2=='no':
+                        break           
        
-#        print('__'*20)
-#        print('1-read book')
-#        print('2-add book')
-#        print('3-buy book')
-#        choiceBook=input('enter ur choice')
-#        if choiceBook=='1':
-#            listBooks=[]
-#            for i in listOfBooks:
-#                listBooks.append(i['name'])
-#            for i in range(len(listBooks)):
-#                print(i+1,'-',listBooks[i])
-#            xChoice=input('enter ur choice :')
-#            print(listOfBooks[int(xChoice)-1]['content'])
-#        elif choiceBook=='2':
-#            newBook={}
-#            name=input('inter the name of the book')
-#            author=input('enter the author of the book')
-#            pages=input('inter the pages of the book')
-#            price=input('enter the price of the book')
-#            content=input('inter the content of the book')
-#            newBook['name']=name
-#            newBook['author']=author
-#            newBook['pages']=pages
-#            newBook['price']=price
-#            newBook['content']=content
-#            listOfBooks.append(newBook)
-#     elif choice=='3':
-#         print("thanks for using our app")
-#         break
-#     elif choice=='4':
-#         isAdmin=input('please enter admin password :')
-#         if isAdmin=='myaw':
-#             counter=1
-#             print('__'*20)
-#             print("all persons")
-#             for i in allpersons:
-#                 print("person",":",counter)
-#             for key in i:
-#                 print("   ",key,":",i[key])
-#                 counter+=1
-#         else:
-#            print('wrong pass') 
-#     else:
-#         print('wrong choice')
+       print('__'*20)
+       print('1-read book')
+       print('2-add book')
+       print('3-buy book')
+       choiceBook=input('enter ur choice')
+       if choiceBook=='1':
+           listBooks=[]
+           for i in listOfBooks:
+               listBooks.append(i['name'])
+           for i in range(len(listBooks)):
+               print(i+1,'-',listBooks[i])
+           xChoice=input('enter ur choice :')
+           print(listOfBooks[int(xChoice)-1]['content'])
+       elif choiceBook=='2':
+           newBook={}
+           name=input('inter the name of the book')
+           author=input('enter the author of the book')
+           pages=input('inter the pages of the book')
+           price=input('enter the price of the book')
+           content=input('inter the content of the book')
+           newBook['name']=name
+           newBook['author']=author
+           newBook['pages']=pages
+           newBook['price']=price
+           newBook['content']=content
+           listOfBooks.append(newBook)
+    elif choice=='3':
+        print("thanks for using our app")
+        break
+    elif choice=='4':
+        isAdmin=input('please enter admin password :')
+        if isAdmin=='myaw':
+            counter=1
+            print('__'*20)
+            print("all persons")
+            for i in allpersons:
+                print("person",":",counter)
+            for key in i:
+                print("   ",key,":",i[key])
+                counter+=1
+        else:
+           print('wrong pass') 
+    else:
+        print('wrong choice')
 
 
 # testStr='hello world'
@@ -556,14 +556,14 @@
 # print(person1.city)     
 
 
-class Person():
+# class Person():
 
-    def __init__(self, name, phone,age, city,pass): 
-        self.name=name
-        self.phone=phone
-        self.age=age
-        self.city=city
-        self.passwoord=pass
+#     def __init__(self, name, phone,age, city,pass): 
+#         self.name=name
+#         self.phone=phone
+#         self.age=age
+#         self.city=city
+#         self.passwoord=pass
     
 #     def nameAndPhone(self):
 #         return f'name:{self.name},phone:{self.phone}'
@@ -578,7 +578,7 @@ class Person():
 # print(Person1.nameAndPhone())
 # print(Person2.eatingWith('malak'))
 
-while True:
+
     
 
 
